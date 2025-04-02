9f01f6343ddc122f35d9b4eb15146cf459e0ad04
Sakshi Antala <sakshi@saasinnova.com>
2025-04-02 16:59:19 +0530

    optimize radio button

--- RadioButtonSelect.tsx
+++ RadioButtonSelect.tsx
@@ -4,6 +4,7 @@ import { RadioButton } from "primereact/radiobutton";
 import { IFormFieldType } from "../../../../library/utilities/constant";
 import { IFormProps } from "../formInterface/forms.model";
 import { FormFieldError } from "../formFieldError/FormFieldError";
+import { useMemo } from "react";
 
 export const RadioButtonSelect = (props: IFormProps) => {
   const { attribute, form, fieldType } = props;
@@ -14,32 +15,23 @@ export const RadioButtonSelect = (props: IFormProps) => {
     formState: { errors },
   } = useFormContext();
 
-  const getClassNames = () => {
-    let labelClassName = "";
-    let fieldClassName = "";
-    let divClassName = "";
-
+  const { labelClassName, fieldClassName, divClassName } = useMemo(() => {
     switch (fieldType) {
       case IFormFieldType.NO_LABEL:
-        labelClassName = "";
-        fieldClassName = "field p-fluid";
-        divClassName = "grid";
-        break;
       case IFormFieldType.TOP_LABEL:
-        labelClassName = "";
-        fieldClassName = "field p-fluid";
-        divClassName = "grid";
-        break;
+        return {
+          labelClassName: "",
+          fieldClassName: "field p-fluid",
+          divClassName: "grid",
+        };
       default:
-        labelClassName = "col-12 mt-2 mb-3 md:col-3 md:mb-0";
-        fieldClassName = "field grid";
-        divClassName = "col-12 mt-2 md:col-9 grid relative";
-        break;
+        return {
+          labelClassName: "col-12 mt-2 mb-3 md:col-3 md:mb-0",
+          fieldClassName: "field grid",
+          divClassName: "col-12 mt-2 md:col-9 grid relative",
+        };
     }
-
-    return { labelClassName, fieldClassName, divClassName };
-  };
-  const { labelClassName, fieldClassName, divClassName } = getClassNames();
+  }, [fieldType]);
 
   const labelElement = (
     <label htmlFor={attribute} className={labelClassName}>
@@ -54,38 +46,37 @@ export const RadioButtonSelect = (props: IFormProps) => {
     <div className={fieldClassName}>
       {fieldType !== IFormFieldType.NO_LABEL && labelElement}
       <div className={divClassName}>
-        {options &&
-          options.map((option) => {
-            return (
-              <div className="flex p-2" key={option.value} style={widthStyle}>
-                <Controller
-                  name={attribute}
-                  control={control}
-                  rules={inputValidator(form[attribute].rules, label)}
-                  render={({ field }) => {
-                    return (
-                      <>
-                        <RadioButton
-                          onChange={(e) => field.onChange(e.value)}
-                          value={option.value}
-                          inputId={`${field.name}_${option.value}`}
-                          checked={option.value === field.value ? true : false}
-                          disabled={disabled}
-                          className={errors[attribute] ? "p-invalid" : ""}
-                        />
-                        <label
-                          htmlFor={`${field.name}_${option.value}`}
-                          className="ml-2"
-                        >
-                          {option.label}
-                        </label>
-                      </>
-                    );
-                  }}
-                />
-              </div>
-            );
-          })}
+        {options?.map((option) => {
+          return (
+            <div className="flex p-2" key={option.value} style={widthStyle}>
+              <Controller
+                name={attribute}
+                control={control}
+                rules={inputValidator(form[attribute].rules, label)}
+                render={({ field }) => {
+                  return (
+                    <>
+                      <RadioButton
+                        onChange={(e) => field.onChange(e.value)}
+                        value={option.value}
+                        inputId={`${field.name}_${option.value}`}
+                        checked={option.value === field.value ? true : false}
+                        disabled={disabled}
+                        className={errors[attribute] ? "p-invalid" : ""}
+                      />
+                      <label
+                        htmlFor={`${field.name}_${option.value}`}
+                        className="ml-2"
+                      >
+                        {option.label}
+                      </label>
+                    </>
+                  );
+                }}
+              />
+            </div>
+          );
+        })}
         <FormFieldError data={{ errors, name: attribute }} />
       </div>
     </div>
